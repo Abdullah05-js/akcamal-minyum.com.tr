43fc59653ecefe6619150773a4e1aa2dd94065eb
thodex <abdallhameed2005@gmail.com>
2024-10-29 17:03:07 +0300

    v1.2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <a href="https://api.whatsapp.com/send?phone=905393577701" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary bg-primary outline-none ${styles} rounded-[10px]`}>
+    <a href="https://api.whatsapp.com/send?phone=905326896652" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary bg-primary outline-none ${styles} rounded-[10px]`}>
      WhatsApp
     </a>
   )
